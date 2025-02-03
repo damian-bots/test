857b548bfbf7185dc62432fa739297dcb295cce9
damian-bots <tosu0791@gmail.com>
2025-02-04 00:56:14 +0530

    Update code.py

--- code.py
+++ code.py
@@ -30,20 +30,20 @@ logger = logging.getLogger(__name__)
 
 # --- API Setup ---
 # Spotify API
-SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
-SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+SPOTIFY_CLIENT_ID = "95f4f5c6df5744698035a0948e801ad9"
+SPOTIFY_CLIENT_SECRET = "4b03167b38c943c3857333b3f5ea95ea"
 spotify_credentials_manager = SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=spotify_credentials_manager)
 
 # Telegram Bot API
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = "7741293072:AAEiWZSyFz1V39uQYbHEk10BTUoPYiUxyS4"
 updater = Updater(BOT_TOKEN)
 dispatcher = updater.dispatcher
 
 # Telegram User Client
-API_ID = os.getenv("API_ID")
-API_HASH = os.getenv("API_HASH")
-SESSION = os.getenv("SESSION")
+API_ID = 24620300
+API_HASH = "9a098f01aa56c836f2e34aee4b7ef963"
+SESSION = "BQGC-ccAWwiLhsvQpd7jdiZmReOM8zqPb-Ra9Je4THqbS0mq6jYnFQS-K9LDpz-YHqQUMsLOuLqgHdD1edUMQmQhPyjF38VcurIT2b4LYZVeFSzfjXoUKwOUsGIFzlvfo6bUrzM7ouhcP86quH4IR2LfueSXWJdDnvu8qS3Gm5-d7W2M13vebJ5NfEymsUAJW6zjR9IusQ8f5Nei7UzUZsl1ww6cI8T_gcu6wiSP1LfWjOVQ97G6ab3-2jxJ-nPlA3cGi8q5dHhIB81LRZymSq03oSXEMJihIOXPfj2pI0XGlI-Y85nC60hSwXuV5Y02_VmAZ_j157Co39b2r77gjfXzvfMcjgAAAAG0-mQtAA"
 pytgcalls = PyTgCalls(
         telegram.Client(
         "assistant",
