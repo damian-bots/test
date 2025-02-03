f132eefbe484d05dcedcbdc896b77eb28c58145a
damian-bots <tosu0791@gmail.com>
2025-02-04 01:31:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,9 +17,9 @@ import json
 import subprocess
 import ffmpeg
 from pytgcalls import PyTgCalls
-from pytgcalls.types import AudioPiped, AudioQuality
-from pytgcalls import StreamAudioEnded
-from pytgcalls.types import HighQualityAudio
+from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
+from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
+from pytgcalls.types.stream import StreamAudioEnded
 
 load_dotenv()
 
