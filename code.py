f2b40dd5ce6546283f665c2b11a18732292fba15
damian-bots <tosu0791@gmail.com>
2025-02-04 01:11:44 +0530

    Update code.py

--- code.py
+++ code.py
@@ -18,7 +18,7 @@ import subprocess
 import ffmpeg
 from pytgcalls import PyTgCalls
 from pytgcalls.types import AudioPiped, AudioQuality
-from pytgcalls.types import InputAudioStream
+from py_tgcalls.stream import InputAudioStream
 from pytgcalls.types import HighQualityAudio
 
 load_dotenv()
